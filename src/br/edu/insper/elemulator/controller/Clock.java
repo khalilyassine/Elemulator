ff94e9a59b81f3a016dfdcb105a61bf9a8f4332b
khalilyassine <khalilyyassine@hotmail.com>
2016-11-17 09:05:47 -0200

    Clock Semi-completo

--- src/br/edu/insper/elemulator/controller/Clock.java
+++ src/br/edu/insper/elemulator/controller/Clock.java
@@ -1,7 +1,7 @@
 package br.edu.insper.elemulator.controller;
 
 import java.util.Timer;
-import java.util.TimerTask;
+
 
 
 public class Clock {
@@ -9,19 +9,15 @@ public class Clock {
 	Timer tempo;
 	int[] k = new int[]{0,1,1,0,1,1,0,1,0,1};
 for (int i = 0; i<10; i++){
-	System.out.println(k[i]);
-	Thread.sleep(1000);
+	 tempo = new Timer();
+	tempo.schedule(new Tempo() {
+	    @Override
+	    public void run() {
 
-	
-	}}}
+	    	    System.out.println(k[i]);
 
-/*tempo = new Timer();
-tempo.schedule(new TimerTask() {
-    @Override
-    public void run() {
+	    }
+	}, 750);
+}}}
 
-    	    System.out.println("banana");
 
-    }
-}, 750);
-*/
